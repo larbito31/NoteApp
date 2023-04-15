cb51178b4910d237e4e917b68526d0c9c11779d4
larbito31 <boukhobza.larbi31@gmail.com>
2023-04-15 03:26:27 +0200

    added get, update, save and delete functions

---
+++ controller/noteController.js
@@ -0,0 +1,16 @@
+export function getAllnotes(req, res) {
+    res.send('get all Notes');
+  }
+
+
+  export function saveNote(req, res) {
+    res.send('save note .........');
+  }
+
+  export function updateNote(req, res) {
+    res.send('update note .........');
+  }
+
+  export function deleteNote(req, res) {
+    res.send('delete note .........');
+  }
--- route/noteRoute.js
+++ route/noteRoute.js
@@ -1,11 +1,18 @@
 import express from 'express';
+import {getAllnotes} from '../controller/noteController.js';
+import {saveNote} from '../controller/noteController.js';
+import {updateNote} from '../controller/noteController.js';
+import {deleteNote} from '../controller/noteController.js';
+
 const router = express.Router();
 
 
+
 // define the home page route
-router.get('/notes', (req, res) => {
-    res.send('get All notes')
-  })
+router.get('/notes', getAllnotes)
+router.post('/notes/save', getAllnotes)
+router.get('/notes/update', getAllnotes)
+router.delete('/notes/delete', getAllnotes)
 
   export default router;
 
