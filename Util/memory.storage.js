fa8d502f240100f4de806cd455977b600434d159
larbito31 <boukhobza.larbi31@gmail.com>
2023-04-16 20:51:12 +0200

    Generator and Memorystage Added

---
+++ Util/memory.storage.js
@@ -0,0 +1,34 @@
+import MemoryStorage from 'memorystorage'
+
+// here, the MemoryStorage function is available
+const myStorage = new MemoryStorage('note-app');
+
+var store = MemoryStorage('my-store');
+var global = MemoryStorage();
+
+console.log(store.id); // alerts 'my-store'
+console.log(global.id); // alerts 'global'
+
+
+export const getKeys = () => {
+var keys = []
+    for (var i=0; i<store.length; i++) {
+        var key = store.key(i);
+        keys.push(key);
+    }
+return keys;
+}
+
+
+export const getValues = () => {
+    var values = []
+        for (var i=0; i<store.length; i++) {
+            var key =store.key(i);
+            var value = store.getItem(key);
+            values.push(value);
+        }
+    return values;
+    }
+
+
+export default myStorage;
