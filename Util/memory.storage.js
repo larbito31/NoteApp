097ed673ea7fdb9259474749d82aefe8e525dc2f
larbito31 <boukhobza.larbi31@gmail.com>
2023-04-18 02:15:35 +0200

    Get, Put,Post added

--- Util/memory.storage.js
+++ Util/memory.storage.js
@@ -1,16 +1,16 @@
 import MemoryStorage from 'memorystorage';
 
-const myStorage = new MemoryStorage('note-app');
+const store = new MemoryStorage('note-app');
 
-export const store = myStorage;
+// export const store = myStorage;
 // var global = MemoryStorage();
 // console.log(store.id); // alerts 'note-app'
 // console.log(global.id); // alerts 'global'
 
 export const getKeys = () => {
   var keys = [];
-  for (var i=0; i<myStorage.length; i++) {
-    var key = myStorage.key(i);
+  for (var i=0; i<store.length; i++) {
+    var key = store.key(i);
     keys.push(key);
   }
   return keys;
@@ -18,9 +18,9 @@ export const getKeys = () => {
 
 export const getValues = () => {
   var values = [];
-  for (var i=0; i<myStorage.length; i++) {
-    var key = myStorage.key(i);
-    var value = myStorage.getItem(key);
+  for (var i=0; i<store.length; i++) {
+    var key = store.key(i);
+    var value = store.getItem(key);
     values.push(value);
   }
   return values;
